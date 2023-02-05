3ce43c0a77ca603d478d36125c796404a60f044a
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2023-02-05 15:18:09 +0530

    Update pythontgbot.py

--- pythontgbot.py
+++ pythontgbot.py
@@ -58,7 +58,6 @@ async def log_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
        is_chat = vick.find({"chat":chat.id, "word": message.text})                 
        for x in is_chat:
            K.append(x['text'])
-       if K:
            hey = random.choice(K)
            is_text = vick.find_one({"chat":chat.id, "text": hey})
            Yo = is_text['check']
@@ -76,8 +75,7 @@ async def log_user(update: Update, context: ContextTypes.DEFAULT_TYPE):
            K = []  
            is_chat = vick.find({"chat":chat.id, "word": message.text})                 
            for x in is_chat:
-               K.append(x['text'])
-           if K:
+               K.append(x['text'])           
                hey = random.choice(K)
                is_text = vick.find_one({"chat":chat.id, "text": hey})
                Yo = is_text['check']
